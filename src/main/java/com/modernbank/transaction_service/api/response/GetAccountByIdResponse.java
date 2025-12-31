df41c8cdfee090300fe1f10c7d20150078b2c0ca
Ataberk Bakir <bakirr.ataberk@gmail.com>
2025-12-31 11:46:12 +0300

    Add no-args constructor to `GetAccountByIdResponse` for improved usability

--- src/main/java/com/modernbank/transaction_service/api/response/GetAccountByIdResponse.java
+++ src/main/java/com/modernbank/transaction_service/api/response/GetAccountByIdResponse.java
@@ -3,10 +3,12 @@ package com.modernbank.transaction_service.api.response;
 import com.modernbank.transaction_service.api.dto.AccountDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class GetAccountByIdResponse {
     AccountDTO account;
